078faa2271f9641ad16c2353c929b189af5452a2
João Mozelli Neto <joaomozelli@hotmail.com>
2020-09-15 18:37:11 -0300

    create routes file

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+//import Landing from './pages/Landing';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            {/*<Route exact path="/" component={ Landing } />*/}
+        </BrowserRouter>
+    );
+}
